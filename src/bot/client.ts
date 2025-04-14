e18a246400179ea31a3f0f7c67a60f163f8b0400
idiax7 <marcinwol321@gmail.com>
2025-04-15 00:27:57 +0200

    add: jsdocs

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -1,18 +1,15 @@
-import {
-  Client,
-  ClientOptions,
-  Collection,
-  ChatInputCommandInteraction,
-  REST,
-  Routes,
-  SlashCommandBuilder,
-} from 'discord.js';
+import { Client, ClientOptions, Collection } from 'discord.js';
 import { CommandInterface } from '../types/Command';
 
 export class BotClient extends Client {
   public commands: Collection<string, CommandInterface> = new Collection();
   public cooldowns: Collection<string, Map<string, number>> = new Collection();
 
+  /**
+   * Creates a new instance of the BotClient class.
+   *
+   * @param {ClientOptions} options - The options to use for the client.
+   */
   constructor(options: ClientOptions) {
     super(options);
   }
