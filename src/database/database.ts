5dadd0beb42e68b813d4f086d0d0a1c70117fbf5
idiax7 <marcinwol321@gmail.com>
2025-04-15 00:28:25 +0200

    add: jsdocs

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,15 @@
 import mongoose from 'mongoose';
 import { logError, logInfo } from '../lib/logger';
 
+/**
+ * Establishes a connection to the database.
+ *
+ * The DATABASE_URI environment variable should be set to the MongoDB connection string.
+ * If the connection is successful, a success message is logged to the console.
+ * If the connection fails, the error is logged to the console.
+ *
+ * @returns {Promise<void>}
+ */
 export default async function connectDatabase() {
   try {
     await mongoose.connect(process.env.DATABASE_URI!);
